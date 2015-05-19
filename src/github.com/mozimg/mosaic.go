322235f8f64248cfcc5d8521b2e4ebcbfffc3975
Anshul Sharma <anshul@tubularlabs.com>
2015-05-19 14:11:45 -0700

    fix formatting issues

--- src/github.com/mozimg/mosaic.go
+++ src/github.com/mozimg/mosaic.go
@@ -1,39 +1,33 @@
 package main
 
-import(
-	"os"
+import (
 	"fmt"
-	_ "io"
-	"sort"
 	"image"
-	_ "io/ioutil"
-    "image/color"
+	"image/color"
 	_ "image/jpeg"
+	_ "io"
+	"os"
+	"sort"
 )
-
 
 // datastructure for sorting image.Image objects
 type sortedMap struct {
-	rgbm map[image.Image]color.RGBA
+	rgbm   map[image.Image]color.RGBA
 	ycbcrm map[image.Image]color.YCbCr
-	i []image.Image
+	i      []image.Image
 }
-
 
 func (sm *sortedMap) Len() int {
 	return len(sm.i)
 }
 
-
 func (sm *sortedMap) Less(j, k int) bool {
 	return sm.ycbcrm[sm.i[j]].Y > sm.ycbcrm[sm.i[k]].Y
 }
 
-
 func (sm *sortedMap) Swap(j, k int) {
 	sm.i[j], sm.i[k] = sm.i[k], sm.i[j]
 }
-
 
 // generate mosaic given a target image, and an array of tile images
 func generateMosaic(target image.Image, tiles []image.Image, rows int, columns int) image.Image {
@@ -43,10 +37,10 @@ func generateMosaic(target image.Image, tiles []image.Image, rows int, columns i
 	x_length, y_length := int(bounds.Max.X/columns), int(bounds.Max.Y/rows)
 
 	// iterate through target image's cells and get tile
-	for x := 0; x < bounds.Max.X - x_length; x += x_length {
-		for y := 0; y < bounds.Max.Y - y_length; y += y_length {
+	for x := 0; x < bounds.Max.X-x_length; x += x_length {
+		for y := 0; y < bounds.Max.Y-y_length; y += y_length {
 			ycbcrImg := target.(*image.YCbCr)
-			cell := ycbcrImg.SubImage(image.Rect(x, x + x_length, y, y + y_length))
+			cell := ycbcrImg.SubImage(image.Rect(x, x+x_length, y, y+y_length))
 			_, averageYCbCr := getAverageColor(cell)
 			tile := getSimilarTile(averageYCbCr, index)
 			fmt.Printf("%T\n", tile)
@@ -55,12 +49,11 @@ func generateMosaic(target image.Image, tiles []image.Image, rows int, columns i
 	return target
 }
 
-
 // get the closest tile for a given value
 func getSimilarTile(value color.YCbCr, index *sortedMap) image.Image {
 	images := index.i
 	for len(images) > 1 {
-		mid := uint32(len(images)/2)
+		mid := uint32(len(images) / 2)
 		if value.Y > index.ycbcrm[images[mid]].Y {
 			images = images[mid:]
 		} else {
@@ -85,7 +78,6 @@ func getImageObject(images []string) []image.Image {
 	return objects
 }
 
-
 // create index mapping image object to its average RGBA value
 func getTileIndex(tiles []image.Image) *sortedMap {
 	index := new(sortedMap)
@@ -107,7 +99,7 @@ func getAverageColor(img image.Image) (color.RGBA, color.YCbCr) {
 	ar, ag, ab, aa := 0.0, 0.0, 0.0, 0.0
 	numPix := float64((bounds.Max.X - bounds.Min.X) * (bounds.Max.Y - bounds.Min.Y))
 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
-		for y:= bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			pixel := img.At(x, y)
 			r, g, b, a := pixel.RGBA()
 			ar += float64(r)
@@ -124,11 +116,10 @@ func getAverageColor(img image.Image) (color.RGBA, color.YCbCr) {
 	return averageRGBA, averageYCbCr
 }
 
-
 // main
 func main() {
-    // read target image and tile images paths from the
-    // command line
+	// read target image and tile images paths from the
+	// command line
 	images := os.Args[1:]
 	objects := getImageObject(images)
 	generateMosaic(objects[0], objects[1:], 10, 10)
