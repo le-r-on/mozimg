9f30406e7c815fff338b1302d11a51b8b12e99d0
Anshul Sharma <anshul@tubularlabs.com>
2015-05-19 14:02:44 -0700

    add logic for iterating through target image cells
    also, isolate color averaging method

--- src/github.com/mozimg/mosaic.go
+++ src/github.com/mozimg/mosaic.go
@@ -36,9 +36,22 @@ func (sm *sortedMap) Swap(j, k int) {
 
 
 // generate mosaic given a target image, and an array of tile images
-func generateMosaic(target image.Image, tiles []image.Image) image.Image {
+func generateMosaic(target image.Image, tiles []image.Image, rows int, columns int) image.Image {
 	index := getTileIndex(tiles)
 	sort.Sort(index)
+	bounds := target.Bounds()
+	x_length, y_length := int(bounds.Max.X/columns), int(bounds.Max.Y/rows)
+
+	// iterate through target image's cells and get tile
+	for x := 0; x < bounds.Max.X - x_length; x += x_length {
+		for y := 0; y < bounds.Max.Y - y_length; y += y_length {
+			ycbcrImg := target.(*image.YCbCr)
+			cell := ycbcrImg.SubImage(image.Rect(x, x + x_length, y, y + y_length))
+			_, averageYCbCr := getAverageColor(cell)
+			tile := getSimilarTile(averageYCbCr, index)
+			fmt.Printf("%T\n", tile)
+		}
+	}
 	return target
 }
 
@@ -60,7 +73,7 @@ func getSimilarTile(value color.YCbCr, index *sortedMap) image.Image {
 // get image objects (color.YCbCr) given file paths
 func getImageObject(images []string) []image.Image {
 	objects := make([]image.Image, len(images))
-	for i, obj := range(images) {
+	for i, obj := range images {
 		file, ferr := os.Open(obj)
 		if ferr == nil {
 			img, _, ierr := image.Decode(file)
@@ -79,29 +92,36 @@ func getTileIndex(tiles []image.Image) *sortedMap {
 	index.rgbm = make(map[image.Image]color.RGBA)
 	index.ycbcrm = make(map[image.Image]color.YCbCr)
 	index.i = make([]image.Image, len(tiles))
-	for ind, tile := range(tiles) {
-		bounds := tile.Bounds()
-        ar, ag, ab, aa := 0.0, 0.0, 0.0, 0.0
-		numPix := float64((bounds.Max.X - bounds.Min.X) * (bounds.Max.Y - bounds.Min.Y))
-        for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			for y:= bounds.Min.Y; y < bounds.Max.Y; y++ {
-                pixel := tile.At(x, y)
-                r, g, b, a := pixel.RGBA()
-                ar += float64(r)
-                ag += float64(g)
-                ab += float64(b)
-				aa += float64(a)
-			}
-		}
-		averageRGBA := color.RGBA{R: uint8(ar/numPix), G: uint8(ag/numPix),
-		                          B: uint8(ab/numPix), A: uint8(aa/numPix)}
-		y, cb, cr := color.RGBToYCbCr(uint8(ar/numPix), uint8(ag/numPix), uint8(ab/numPix))
-		averageYCbCr := color.YCbCr{Y: y, Cb: cb, Cr: cr}
+	for ind, tile := range tiles {
+		averageRGBA, averageYCbCr := getAverageColor(tile)
 		index.rgbm[tile] = averageRGBA
 		index.ycbcrm[tile] = averageYCbCr
 		index.i[ind] = tile
 	}
 	return index
+}
+
+// get average RGBA, YCbCr color values given an image
+func getAverageColor(img image.Image) (color.RGBA, color.YCbCr) {
+	bounds := img.Bounds()
+	ar, ag, ab, aa := 0.0, 0.0, 0.0, 0.0
+	numPix := float64((bounds.Max.X - bounds.Min.X) * (bounds.Max.Y - bounds.Min.Y))
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y:= bounds.Min.Y; y < bounds.Max.Y; y++ {
+			pixel := img.At(x, y)
+			r, g, b, a := pixel.RGBA()
+			ar += float64(r)
+			ag += float64(g)
+			ab += float64(b)
+			aa += float64(a)
+		}
+	}
+	r, g, b, a := uint8(ar/numPix), uint8(ag/numPix), uint8(ab/numPix), uint8(aa/numPix)
+	averageRGBA := color.RGBA{R: r, G: g, B: b, A: a}
+	y, cb, cr := color.RGBToYCbCr(r, g, b)
+	averageYCbCr := color.YCbCr{Y: y, Cb: cb, Cr: cr}
+
+	return averageRGBA, averageYCbCr
 }
 
 
@@ -111,5 +131,5 @@ func main() {
     // command line
 	images := os.Args[1:]
 	objects := getImageObject(images)
-	generateMosaic(objects[0], objects[1:])
+	generateMosaic(objects[0], objects[1:], 10, 10)
 }
