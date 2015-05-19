6a0b3b4dd10879735dad01ae4fdaaa83c48229ee
Roman Bodnarchuk <bodnarchuk.roman@gmail.com>
2015-05-19 15:08:20 -0700

    Added missed file

---
+++ src/github.com/mozimg/util.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "github.com/gocraft/web"
+    "github.com/polds/imgbase64"
+    _ "github.com/franela/goreq"
+    "io"
+    "html/template"
+    _ "fmt"
+    _ "log"
+    "bytes"
+    "image"
+    _ "image/jpeg"
+    "image/png"
+    _ "image/color"
+    "bufio"
+)
+
+func getAvgColorFromImg(imageObj image.Image) image.Image {
+    avgColor, _ := getAverageColor(imageObj)
+    avgColorImg := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{32, 32}})
+    for y := 0; y < 32; y++ {
+        for x := 0; x < 32; x++ {
+            avgColorImg.SetRGBA(x, y, avgColor)       
+        }
+    }
+    return avgColorImg
+}
+
+func imageToB64(imageObj image.Image) string {
+    tmp_buf := new(bytes.Buffer)
+    png.Encode(tmp_buf, imageObj)
+    avgColorB64 := imgbase64.FromBuffer(*tmp_buf)
+    return avgColorB64
+}
+
+func displayImgObj(rw web.ResponseWriter, imageObj image.Image) {
+    origImgB64 := imageToB64(imageObj)
+    avgColorB64 := imageToB64(getAvgColorFromImg(imageObj))
+    base_tmpl.Execute(
+        rw,
+        &Context{Image: template.URL(origImgB64), AvgColor: template.URL(avgColorB64)})
+}
+
+func imageFromReader(reader io.Reader) image.Image {
+    img, _, _ := image.Decode(reader)
+    return img
+}
+
+func imageFromBuffer(buffer bytes.Buffer) image.Image {
+    return imageFromReader(bufio.NewReader(&buffer))
+}
